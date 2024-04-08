b792995e5144fd99bb5c8c6598301c68fea77c52
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-04-08 01:15:46 -0300

    fix_deploy

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -5,31 +5,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
 
-export async function GET(req: NextApiRequest) {
+export async function GET() {
   await connectMongoDB();
-  const { userId } = getAuth(req);
-  console.log("user", userId);
   const users = await getAllUsers();
   return NextResponse.json({ users });
 }
 
-// export async function GET(req: NextApiRequest, res: NextApiResponse) {
-//   const { user } = auth();
-//   console.log("user", user);
-
-//   if (!user || !user.publicMetadata || typeof user.publicMetadata.userId !== 'string') {
-//     return new Response("Unauthorized", { status: 401 });
-//   }
-
-//   const userId = user.publicMetadata.userId;
-
-//   try {
-//     await connectMongoDB();
-//     const userData = await getUserById(userId);
-
-//     return NextResponse.json({ userData });
-//   } catch (error) {
-//     console.error("Error retrieving user data:", error);
-//     return new Response("Internal Server Error", { status: 500 });
-//   }
-// }
