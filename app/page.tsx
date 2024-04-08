3eeb9c5e2bedfca64713598bf8a90f3ea9929272
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-04-08 01:19:14 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import { auth, currentUser } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default async function Home() {
-  // const user = await currentUser();
-  // const mongoDbUser = user?.publicMetadata.userId as string;
-  // const thisUser = await getUserById(mongoDbUser);
+  const user = await currentUser();
+  const mongoDbUser = user?.publicMetadata.userId as string;
+  const thisUser = await getUserById(mongoDbUser);
 
   return (
     <div>
+      {user && <h1>{thisUser.level}</h1>}
       <Header />
     </div>
   );
