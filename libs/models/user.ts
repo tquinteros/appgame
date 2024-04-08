08c386c2bfa4b5432ea219a0017d65974d156f5d
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-04-07 23:29:10 -0300

    level

--- libs/models/user.ts
+++ libs/models/user.ts
@@ -1,31 +1,34 @@
-
 import mongoose, { Schema, Model, Models } from "mongoose";
 
 const UserSchema = new Schema({
-    clerkId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    username: {
-        type: String,
-        unique: true,
-    },
-    photo: {
-        type: String,
-    },
-    firstName: {
-        type: String,
-    },
-    lastName: {
-        type: String,
-    },
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+  },
+  photo: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  level: {
+    type: Number,
+    default: 1,
+  },
 });
-const User = mongoose.models.User || mongoose.model("User", UserSchema)
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
 export default User;
