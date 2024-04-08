4e1bb7d85b09f5363c8472be4902305d66b02eb9
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-04-08 01:52:10 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 import Header from "@/components/Header/Header";
 import { getUserById } from "@/libs/actions/user.action";
-import { auth, currentUser, UserButton } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { User } from "@/interfaces/User";
+// Define una interfaz para el usuario de MongoDB
 
 export default async function Home() {
-  // const user = await currentUser();
-  // const mongoDbUser = user?.publicMetadata.userId as string;
-  // const thisUser = await getUserById(mongoDbUser);
+  const user = await currentUser();
+  const mongoDbUser = user?.publicMetadata.userId as string;
+  const thisUser = await getUserById(mongoDbUser);
+
+  const mongoDBUser: User | null = thisUser
+    ? {
+        id: thisUser._id.toString(),
+        username: thisUser.username,
+        level: thisUser.level,
+      }
+    : null;
 
   return (
     <div>
-      {/* {user && <h1>{thisUser.level}</h1>} */}
-      <Header />
+      <Header user={mongoDBUser} />
     </div>
   );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,20 @@
 "use client";
-import { useUser } from "@clerk/nextjs";
-import Link from "next/link";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useClerk } from "@clerk/clerk-react";
+import Link from "next/link";
 
-const Header = () => {
-  const { user } = useUser();
+// Define una interfaz para el usuario de MongoDB
+interface MongoDBUser {
+  id: string;
+  username: string;
+  level: number;
+}
+
+interface HeaderProps {
+  user: MongoDBUser | null;
+}
+
+const Header: React.FC<HeaderProps> = ({ user }) => {
   const { signOut } = useClerk();
 
   useEffect(() => {
@@ -15,12 +24,16 @@ const Header = () => {
   return (
     <header className="container mx-auto flex items-center justify-between">
       <h3 className="text-4xl">LOGO</h3>
-      <h3 className="text-4xl">{user?.username}</h3>
+      <h3 className="text-4xl">{user?.level}</h3>
       <ul className="flex gap-4">
         <li>Home</li>
         <li>Home</li>
         <li>Home</li>
-        <button onClick={() => signOut()}>Sign out</button>
+        {user ? (
+          <button onClick={() => signOut()}>Sign out</button>
+        ) : (
+          <Link href="/sign-in">Log in</Link>
+        )}
       </ul>
     </header>
   );
